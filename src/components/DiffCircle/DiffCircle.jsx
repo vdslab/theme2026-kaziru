e28abe143b41ef7994caf68f7780d31e1fe57a30
kazari4 <skidyuk86115@gmail.com>
2026-07-13 18:30:05 +0900

    refactor: 色管理を一元化

--- src/components/DiffCircle/DiffCircle.jsx
+++ src/components/DiffCircle/DiffCircle.jsx
@@ -1,15 +1,5 @@
 import { useState, useRef, useCallback } from "react";
-
-const BAND_COLORS = {
-    "0-399": "#808080",
-    "400-799": "#804000",
-    "800-1199": "#008000",
-    "1200-1599": "#00C0C0",
-    "1600-1999": "#0000FF",
-    "2000-2399": "#C0C000",
-    "2400-2799": "#FF8000",
-    "2800-3199": "#FF0000",
-};
+import { BAND_COLORS } from "../../utils/diffColors";
 
 const TOOLTIP_DELAY_MS = 200;
 const TOOLTIP_OFFSET_Y = 2;
